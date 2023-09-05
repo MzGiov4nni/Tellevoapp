4c21a7849dc1b44cae3e4182b7761745f4f7d4c3
Giovanni <101910812+MzGiov4nni@users.noreply.github.com>
2023-09-05 18:11:35 -0300

    mostrar nombre en home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  nombreUsuario!: string; // Propiedad para almacenar el nombre del usuario
 
   constructor() {}
 
+  ngOnInit() {
+    // Recuperar los datos del usuario desde el Local Storage
+    const usuarioJSON = localStorage.getItem('usuario');
+    if (usuarioJSON) {
+      const usuario = JSON.parse(usuarioJSON);
+      this.nombreUsuario = usuario.nombre;
+    }
+  }
 }
