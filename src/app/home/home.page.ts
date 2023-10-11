c8c39f72254441a33ff4549353f964cb4e00b54c
joalmontes <jo.almontes@duocuc.cl>
2023-10-11 00:13:39 -0300

    version 1.6

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,10 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  nombreUsuario!: string; // Propiedad para almacenar el nombre del usuario
+  nombreUsuario!: string;
   map!: mapboxgl.Map;
-
+  latitud: number = 0; 
+  longitud: number = 0;
   constructor(private router: Router) {}
 
   goToPerfil() {
@@ -19,24 +21,39 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit() {
-    // Recuperar los datos del usuario desde el Local Storage
     const usuarioJSON = localStorage.getItem('usuario');
     if (usuarioJSON) {
       const usuario = JSON.parse(usuarioJSON);
       this.nombreUsuario = usuario.nombre;
     }
+    
+    // Obtiene la ubicación y luego inicializa el mapa
+    this.obtenerLatitudLongitud();
+  }
 
-    // Configurar el token de acceso al crear el mapa
-    this.initializeMap();
+  async obtenerLatitudLongitud() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.latitud = coordinates.coords.latitude;
+      this.longitud = coordinates.coords.longitude;
+      console.log('Latitud:', this.latitud);
+      console.log('Longitud:', this.longitud);
+
+      // Después de obtener la ubicación, inicializa el mapa
+      this.initializeMap();
+    } catch (error) {
+      console.error('Error al obtener la ubicación:', error);
+    }
   }
 
   initializeMap() {
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40],
-      zoom: 9,
-      accessToken: environment.mapboxToken, // Configura el token aquí
+      center: [this.longitud, this.latitud], // Nota que se invierten latitud y longitud
+      zoom: 16,
+      accessToken: environment.mapboxToken,
     });
-  }
+  }  
 }
+
