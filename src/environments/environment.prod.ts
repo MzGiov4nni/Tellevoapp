ee054e11edf8fb6fb296a80b740a9fa270f72ad3
joalmontes <jo.almontes@duocuc.cl>
2023-11-08 20:20:06 -0300

    android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  mapboxToken: 'pk.eyJ1Ijoiam8tYWxtb250ZXMiLCJhIjoiY2xuam9naXg4MTdkdDJsbzJibG9vM2hnNCJ9.712WFIl33YSIArObHq8VAg',
 };
