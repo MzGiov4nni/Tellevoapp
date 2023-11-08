673d4c24bc48d1ad96dbaf76a6f90c05b309d1ec
Gio <101910812+MzGiov4nni@users.noreply.github.com>
2023-11-08 19:49:29 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
   mapboxToken: 'pk.eyJ1Ijoiam8tYWxtb250ZXMiLCJhIjoiY2xuam9naXg4MTdkdDJsbzJibG9vM2hnNCJ9.712WFIl33YSIArObHq8VAg'
 };
